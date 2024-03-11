689e6150ca4f544fc4de9b5f2976f73864d09696
Demetrovichz <pugehok322@gmail.com>
2024-03-11 21:23:12 +0300

    cleanded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen flex-row">
+      <div className="m-auto">
+      <h1 className="text-white font-semibold italic text-2xl">Сервис автоматизации контроля очереди</h1>
+      <span className="text-white p-">Наше решение поможет вам обслуживать большее количество гостей  в загруженное время.</span>
+      </div>
+      <div className="m-auto">
+      <h1 className="text-white font-semibold italic text-3xl">[IMG]</h1>
+      </div>
+  
     </main>
   );
 }

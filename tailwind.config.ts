ada3ce47c0975e3023f03f62634e9f8f038945a4
Ernest <ernest20145@gmail.com>
2024-03-11 20:32:03 +0200

    changed bg color, also configured tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,107 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        main: {
+          white: "F1F1FF",
+          slate: "#141414",
+          lightgray: "#666666",
+          gray: "#454545",
+          green: "#37C034",
+          red: "#F75D5D",
+        },
+      },
+      fontSize: {
+        h1: [
+          "6rem",
+          {
+            lineHeight: "6.75rem",
+          },
+        ],
+        h2: [
+          "4.75rem",
+          {
+            lineHeight: "5.25rem",
+          },
+        ],
+        h3: [
+          "3.5rem",
+          {
+            lineHeight: "3rem",
+          },
+        ],
+        h4: [
+          "3.25rem",
+          {
+            lineHeight: "3.5rem",
+          },
+        ],
+        h5: [
+          "2.875rem",
+          {
+            lineHeight: "3.2rem",
+          },
+        ],
+        h6: [
+          "2.5rem",
+          {
+            lineHeight: "2rem",
+          },
+        ],
+        p1: [
+          "2.25rem",
+          {
+            lineHeight: "1.375em",
+            letterSpacing: "-0.015625rem",
+          },
+        ],
+        p2: [
+          "1.75rem",
+          {
+            lineHeight: "1.375em",
+            letterSpacing: "-0.015625rem",
+          },
+        ],
+        p3: [
+          "1.5rem",
+          {
+            lineHeight: "1.375em",
+            letterSpacing: "-0.015625rem",
+          },
+        ],
+        p4: [
+          "1.25rem",
+          {
+            lineHeight: "1.375em",
+            letterSpacing: "-0.015625rem",
+          },
+        ],
+        p5: [
+          "1rem",
+          {
+            lineHeight: "1.375em",
+            letterSpacing: "-0.015625rem",
+          },
+        ],
+        p6: [
+          "0.75rem",
+          {
+            lineHeight: "1rem",
+            letterSpacing: "-0.015625rem",
+          },
+        ],
+        p7: [
+          "0.625rem",
+          {
+            lineHeight: "1rem",
+            letterSpacing: "-0.015625rem",
+          },
+        ],
       },
     },
+    fontFamily: {},
   },
+
   plugins: [],
 };
 export default config;
